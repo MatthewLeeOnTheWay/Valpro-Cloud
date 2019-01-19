3c41c7bb9e00aeb09df711cbe3c651ed27e50846
李建幸 <2452043851@qq.com>
2019-01-19 14:12:44 +0800

    some fixed problem

--- Micro-Service-Resource01/src/main/java/com/microservice/valpro/resource01/MicroServiceResource01Application.java
+++ Micro-Service-Resource01/src/main/java/com/microservice/valpro/resource01/MicroServiceResource01Application.java
@@ -3,6 +3,7 @@ package com.microservice.valpro.resource01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MicroServiceResource01Application {
 
